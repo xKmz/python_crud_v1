1826fca80d24338064715afacfdafc458fe84e7a
Rene Jr <reneagjunior@gmail.com>
2019-03-10 21:06:02 -0300

    ajustes e melhorias

--- app.py
+++ app.py
@@ -11,112 +11,170 @@ cursor = conn.cursor()
 
 def cadastro():
         print ' '
-        print '########################'
-        print '#    Digite o Nome:    #'
-        print '########################'
+        print '##################################################################'
+        print '#                        Digite o Nome:                          #'
+        print '##################################################################'
         print ' '
         nome = raw_input()
         print ' '
-        print '########################'
-        print '#    Digite a Idade:   #'
-        print '########################'
+        print '##################################################################'
+        print '#                        Digite a Idade:                         #'
+        print '##################################################################'
         print ' '
         idade = raw_input()
         print ' '
-        print '########################'
-        print '#    Digite o Email:   #'
-        print '########################'
+        print '##################################################################'
+        print '#                        Digite o Email:                         #'
+        print '##################################################################'
         print ' '
         email = raw_input()
-        cursor.execute("INSERT INTO clientes (nome, idade, email) VALUES ('%s', '%s', '%s')" % (nome, idade, email))
-        conn.commit()
-        print ' '
-        print ('Os dados de %s foram adicionados com sucesso!') % (nome)
-        
 
+        try:
+            cursor.execute("INSERT INTO clientes (nome, idade, email) VALUES ('%s', '%s', '%s')" % (nome, idade, email))
+            conn.commit()
+            print ' '
+            print ('Os dados de %s foram adicionados com sucesso!') % (nome)
+
+        except:
+            print ' '
+            print "Falha ao acessar o banco de dados."
+            print "Verifique as configurações de acesso."
+            print ' '
+        
 def lista():
         print ' '
-        print '########################'
-        print '#      Clientes:       #'
-        print '########################'
+        print '##################################################################'
+        print '#                           Clientes:                            #'
+        print '##################################################################'
         print ' '
-        cursor.execute('SELECT * FROM clientes')
-        for cliente in cursor.fetchall():
-            print 'Nome: %s, Idade: %s, Email: %s' % (cliente[1], cliente[2], cliente[3])
+
+        try:
+            cursor.execute('SELECT * FROM clientes')
+            for cliente in cursor.fetchall():
+                print 'Nome: %s, Idade: %s, Email: %s' % (cliente[1], cliente[2], cliente[3])
+        
+        except:
+            print ' '
+            print "Falha ao acessar o banco de dados."
+            print "Verifique as configurações de acesso."
+            print ' '
 
 def remove():
     print ' '
-    print '#####################################################'
-    print '#    Digite o nome que você gostaria de remover:    #'
-    print '#####################################################'
+    print '##################################################################'
+    print '#          Digite o nome que você gostaria de remover:           #'
+    print '##################################################################'
     print ' '
     nome = raw_input()
-    sql_delete_query = "DELETE FROM clientes WHERE nome = %s"
-    cursor.execute(sql_delete_query, (nome,))
-    conn.commit()
-    print ' '
-    print ('Os dados de %s foram removidos com sucesso!') % (nome)
-    print ' '
+
+    try:
+        sql_select_query = "SELECT * FROM clientes WHERE nome = %s"
+        cursor.execute(sql_select_query, (nome, ))
+        cliente = cursor.fetchone()
+        
+        if (nome in cliente):
+            try:
+                sql_delete_query = "DELETE FROM clientes WHERE nome = %s"
+                cursor.execute(sql_delete_query, (nome,))
+                conn.commit()
+                print ' '
+                print ('Os dados de %s foram removidos com sucesso!') % (nome)
+                print ' '
+
+            except:
+                    print ' '
+                    print "Falha ao acessar o banco de dados."
+                    print "Verifique as configurações de acesso."
+                    print ' '
+
+    except (Exception, psycopg2.Error) as error:
+        print ' '
+        print("Poxa! O nome não existe. Tente novamente :(")
+        print ' '
 
 def alterar():
     print ''
     print 'Digite o nome do cadastro que você gostaria de alterar?'
     print ''
     nome = raw_input()
+    print ''
 
-    sql_select_query = "SELECT * FROM clientes WHERE nome = %s"
-    cursor.execute(sql_select_query, (nome, ))
-    cliente = cursor.fetchone()
+    try:
+        sql_select_query = "SELECT * FROM clientes WHERE nome = %s"
+        cursor.execute(sql_select_query, (nome, ))
+        cliente = cursor.fetchone()
+        
+        if (nome in cliente):
+            escolha = ''
+            while(escolha != '0'):
+                print ' '
+                print '##################################################################'
+                print '#                                                                #'
+                print '#        Qual das opções você gostaria de atualizar?             #'
+                print '#                                                                #'
+                print '#        1) Nome 2) Idade 3) Email 0) Voltar ao Menu             #'
+                print '#                                                                #'
+                print '##################################################################'
+                print ' '
+                escolha = raw_input()
+                print ' '
 
-    if (nome in cliente):
-        escolha = ''
-        while(escolha != '0'):
-            print '##################################################################'
-            print '#                                                                #'
-            print '#        Qual das opções você gostaria de atualizar?             #'
-            print '#                                                                #'
-            print '#        1) Nome 2) Idade 3) Email 0) Voltar ao Menu             #'
-            print '#                                                                #'
-            print '##################################################################'
-            print ' '
-            escolha = raw_input()
-            print ' '
+                if(escolha == '1'):
+                    print ' '
+                    print 'Digite o novo nome:'
+                    print ' '
+                    novo_nome = raw_input()
+                    
+                    try:
+                        sql_update_query = "UPDATE clientes SET nome = %s WHERE id = %s"
+                        cursor.execute(sql_update_query, (novo_nome, cliente[0]))
+                        conn.commit()
+                    
+                    except:
+                        print ' '
+                        print "Falha ao acessar o banco de dados."
+                        print "Verifique as configurações de acesso."
+                        print ' '
 
-            if(escolha == '1'):
-                print ' '
-                print 'Digite o novo nome:'
-                print ' '
-                novo_nome = raw_input()
-                
-                sql_update_query = "UPDATE clientes SET nome = %s WHERE id = %s"
-                cursor.execute(sql_update_query, (novo_nome, cliente[0]))
-                conn.commit()
-                            
-            elif(escolha == '2'):
-                print ' '
-                print 'Digite a nova idade:'
-                print ' '
-                nova_idade = raw_input()
-                
-                sql_update_query = "UPDATE clientes SET idade = %s WHERE id = %s"
-                cursor.execute(sql_update_query, (nova_idade, cliente[0]))
-                conn.commit()
+                elif(escolha == '2'):
+                    print ' '
+                    print 'Digite a nova idade:'
+                    print ' '
+                    nova_idade = raw_input()
+                    
+                    try:
+                        sql_update_query = "UPDATE clientes SET idade = %s WHERE id = %s"
+                        cursor.execute(sql_update_query, (nova_idade, cliente[0]))
+                        conn.commit()
+                    
+                    except:
+                        print ' '
+                        print "Falha ao acessar o banco de dados."
+                        print "Verifique as configurações de acesso."
+                        print ' '
 
-            elif(escolha == '3'):
-                print ' '
-                print 'Digite o novo email:'
-                print ' '
-                novo_email = raw_input()
-                
-                sql_update_query = "UPDATE clientes SET email = %s WHERE id = %s"
-                cursor.execute(sql_update_query, (novo_email, cliente[0]))
-                conn.commit()
+                elif(escolha == '3'):
+                    print ' '
+                    print 'Digite o novo email:'
+                    print ' '
+                    novo_email = raw_input()
+                    
+                    try:
+                        sql_update_query = "UPDATE clientes SET email = %s WHERE id = %s"
+                        cursor.execute(sql_update_query, (novo_email, cliente[0]))
+                        conn.commit()
 
-    else :
+                    except:
+                        print ' '
+                        print "Falha ao acessar o banco de dados."
+                        print "Verifique as configurações de acesso."
+                        print ' '
+
+    except (Exception, psycopg2.Error) as error:
         print ' '
-        print 'Poxa! Este nome não existe :('
+        print("Poxa! O nome não existe :(")
         print ' '
-   
+
 def procurar():
     print ' '
     print 'Quem você gostaria de procurar?'
@@ -133,7 +191,7 @@ def procurar():
 
     except (Exception, psycopg2.Error) as error:
         print ' '
-        print("O nome não existe")
+        print("O nome não existe. Procure por outro nome :D")
         print ' '
 
 def menu():
